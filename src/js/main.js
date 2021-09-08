3e8cfd616e33999dd37528eaab04c9a65ffc0366
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-09-08 13:08:57 -0500

    format score and comments to use commas

--- src/js/main.js
+++ src/js/main.js
@@ -89,7 +89,8 @@ $("#comments-list").html(html).find("a").attr("target","_blank")}}
 function _defineProperty(obj,key,value){if(key in obj){Object.defineProperty(obj,key,{value:value,enumerable:true,configurable:true,writable:true})}else{obj[key]=value}return obj}class StoryComp{constructor(a_apiResponse){this.by=null;this.descendants=null;this.id=null;this.kids=[];this.score=null;this.time=null;this.title=null;this.type=null;this.url=null;for(const key of Object.keys(a_apiResponse)){this[key]=a_apiResponse[key]}this.siteUrl=`https://news.ycombinator.com/item?id=${this.id}`;this.dt=DateTime.fromSeconds(this.time);this.dtDiff=Dates.getDiffNow(this.dt)}/**
      * Generate the card html
      * @returns Story card html string
-     */getCardHtml(){const url=this.url==null?this.siteUrl:this.url;const dtDisplay=Dates.getDiffDisplayString(this.dtDiff);let html=`
+     */getCardHtml(){const url=this.url==null?this.siteUrl:this.url;const dtDisplay=Dates.getDiffDisplayString(this.dtDiff);// add commas to score and descendants
+const scoreDisplay=this.score.toLocaleString();const descendantsDisplay=this.descendants.toLocaleString();let html=`
         <div class="card ${StoryComp.cardElementClass} custom-shadow" data-id=${this.id}>
             <div class="card-body">
                 <h5 class="card-title"><a href="${url}" target="_blank" class="card-story-link">${this.title}</a></h5>
@@ -98,8 +99,8 @@ function _defineProperty(obj,key,value){if(key in obj){Object.defineProperty(obj
             </div>
             <div class="card-footer px-4">
                 <div class="d-flex align-baseline justify-content-between">
-                    <span><i class='bx bx-like'></i>&nbsp;${this.score}</span>
-                    <span><i class='bx bx-comment-detail'></i>&nbsp;${this.descendants}</span>
+                    <span><i class='bx bx-like'></i>&nbsp;${scoreDisplay}</span>
+                    <span><i class='bx bx-comment-detail'></i>&nbsp;${descendantsDisplay}</span>
                 </div>
             </div>
         </div>`;return html}}_defineProperty(StoryComp,"cardElementClass","card-story");
