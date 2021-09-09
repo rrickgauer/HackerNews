a32fc3d0420d0ff3717e983fc10ebb0d90aeea5e
Ryan Rickgauer <rrickgauer1@gmail.com>
2021-09-09 10:30:01 -0500

    create script to start up sass compiler

---
+++ scripts/start-sass.bat
@@ -0,0 +1,7 @@
+:: ----------------------------------------
+:: Start up the SASS compiler
+:: ----------------------------------------
+cd C:\xampp\htdocs\files\HackerNews\src\css
+sass --watch style.scss style.css
+pause
+
